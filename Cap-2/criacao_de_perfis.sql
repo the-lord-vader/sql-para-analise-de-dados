eabbfe7edbe872f579c498b541967959c6d162b4
The Lord Darth Vader <jgama@a5solutions.com>
2024-06-30 21:53:46 -0300

    Create criacao_de_perfis.sql

---
+++ Cap-2/criacao_de_perfis.sql
@@ -0,0 +1,16 @@
+--Suponhamos que tivéssemos uma tabela customers, contendo nome, data de cadastramento, idade e outros atributos. Para criar um histograma por idade, faça o agrupamento(com group by) pelo campo numérico age e conte as ocorrências de customer_id:
+
+SELECT age, count(customer_id) as customers
+FROM customers
+GROUP BY 1;
+
+--Imagine uma base fictícia chamada orders, que tem data, identificador do cleinte(customer_id, identificador do pedido(order_ir e uma quantidade, vamos escrever uma consulta que retorne a distribuição de pedidos por cliente. Isso não pode ser resolvido com uma simples consulta, requer uma etapa de agregação intermediária, que pode ser executada com uma subconsulta. Primeiro conte o número de pedidos feitos por cada customer_id na subconsulta.
+
+SELECT orders, count(*) as num_customers
+FROM 
+(
+  SELECT customer_id, count(order_id) as orders
+  FROM orders
+  GROUP BY 1
+) a
+GROUP BY 1;
